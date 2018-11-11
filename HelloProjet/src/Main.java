045b2fc68276c152e48a4c2d9588689ebdef5bc0
yxf0325 <41341785+yxf0325@users.noreply.github.com>
2018-11-11 20:54:32 +0800

    Update Main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,9 +5,9 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-		System.out.println("HelloWorld!");
-		System.out.println("HelloWorld!");
-		System.out.println("HelloWorld!");
+	System.out.println("HelloWorld!");
+	System.out.println("HelloWorld!");
+	System.out.println("HelloWorld!");
 
     }
 }
